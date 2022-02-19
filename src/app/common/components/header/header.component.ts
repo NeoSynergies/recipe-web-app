998973272b4efe932d3d91447a9ca6208d386be5
Joan Pol Cosp <neosynergiesoa@gmail.com>
2022-02-19 14:05:53 +0100

    Shopping list header button notifications functionality

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { ShoppingListService } from '../../services/shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  shoppingListElements = [];
+  constructor(
+    private shoppingListService: ShoppingListService
+  ) { }
 
-  constructor() { }
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.shoppingListService.shoppingListElements
+      .subscribe(elements => this.shoppingListElements = elements);
+  }
 
 }
--- src/app/common/services/shopping-list/shopping-list.service.ts
+++ src/app/common/services/shopping-list/shopping-list.service.ts
@@ -7,7 +7,7 @@ import { catchError, tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ShoppingListService {
-  shoppingListElements: BehaviorSubject<any> = new BehaviorSubject({});
+  shoppingListElements: BehaviorSubject<any> = new BehaviorSubject([]);
 
   constructor(
     private http: HttpClient
@@ -16,12 +16,15 @@ export class ShoppingListService {
   addValueToShoppingList(values: any, category: string): Observable<any> {
     // we assign the id if the element have it and if not we generate one
     let valueId: any = values.id ? values.id : this.generateId();
+    values.id = valueId;
     return this.http.put('http://localhost:3000/' + category + '/' + valueId, {
       [valueId]: values
     })
       .pipe(
         tap(
-          (values) => this.shoppingListElements.next({...this.shoppingListElements.getValue(), [valueId]: values[valueId]}),
+          (values) => {
+            this.shoppingListElements.next([...this.shoppingListElements.getValue(), values[valueId]])
+          },
           (err) => {
             console.log('El error');
             console.log(err);
@@ -30,6 +33,7 @@ export class ShoppingListService {
         )
       );
   }
+
 
   /*updateShoppingListProducts(): Observable<string> {
 
